380e05fec0d29f034a19ef9e24a6b44490b2d6db
Tommy G <tommy.tpg03@gmail.com>
2025-03-01 18:46:30 -0500

    progress towards easier installation

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
+# Install dependencies
+yum install npm nodejs python-is-python3 nginx certbot python3-certbot-nginx -y
+
+# Run python script to generate usefuls
+python src/install.py
+
+# Copy API configs and spin up API server
+cp config.json api-server/config.json
+cd api-server
 npm install
-cd wikimaker-web-application
+nohup node server.js & :
+
+# Run Python script to convert config.json to .env for frontend server
+cd ../wikimaker-web-application
 npm install
 npm run build
+
+# Return to starting directory
 cd ..
-nohup node server.js & :
---
+++ src/install.py
@@ -0,0 +1,57 @@
+import json
+import os
+
+nginx_template = open('src/nginx-template.txt', 'r').read().strip()
+env_template = open('src/env-template.txt', 'r').read().strip()
+manual_template = open('src/manual-command-template.txt', 'r').read().strip()
+
+config = json.loads(open('config.json', 'r').read())
+api_domain = config["apiDomain"]
+website_domains = config["websiteDomains"]
+main_domain = website_domains.split(" ")[0]
+full_main_domain = f'https://{main_domain}'
+debug = str(config["debug"]).lower()
+port = str(config["port"])
+install_directory = os.getcwd()
+nginx_config_file_name = config["nginxConfigFile"]
+
+nginx_template = nginx_template.replace("[WEBSITE_DOMAINS]", website_domains)\
+    .replace("[MAIN_DOMAIN]", main_domain)\
+    .replace("[API_DOMAIN]", api_domain)\
+    .replace("[API_LOCAL_PORT]", port)\
+    .replace("[INSTALL_DIRECTORY]", install_directory)
+
+env_template = env_template.replace('[FULL_MAIN_DOMAIN]', full_main_domain)\
+    .replace('[DEBUG_MODE]', debug)
+
+dash_d_all_domains = ''
+for link in website_domains.split(' '):
+    if len(dash_d_all_domains) > 0:
+        dash_d_all_domains += " "
+    dash_d_all_domains += f'-d {link}'
+dash_d_all_domains += f' -d {api_domain}'
+
+manual_template = manual_template.replace('[DASH_D_ALL_DOMAINS]', dash_d_all_domains)
+
+env_file = open('wikimaker-web-application/.env', 'w')
+env_file.write(env_template)
+env_file.close()
+
+# nginx_file = open('nginx-config.txt', 'w')
+# nginx_file.write(nginx_template)
+# nginx_file.close()
+
+# manual_file = open('manual-commands.txt', 'w')
+# manual_file.write(manual_template)
+# manual_file.close()
+
+os.chdir('..')
+print(os.getcwd())
+os.system(f'chmod 777 -R {os.getcwd()}')
+os.system(f'chown nginx:nginx -R {os.getcwd()}')
+
+nginx_conf_file = open(f'/etc/nginx/conf.d/{nginx_config_file_name}', 'a')
+nginx_conf_file.write(f'\n\n{nginx_template}')
+
+for command in manual_template.split('\n'):
+    os.system(command)
